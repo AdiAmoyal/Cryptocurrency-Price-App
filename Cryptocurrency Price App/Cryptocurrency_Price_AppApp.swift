804995210fa73853b20d38fc057bfd4175b245a1
adiamoyal <adi.amoyal02@gmail.com>
2023-02-12 17:34:28 +0200

    Initial Commit

---
+++ Cryptocurrency Price App/Cryptocurrency_Price_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cryptocurrency_Price_AppApp.swift
+//  Cryptocurrency Price App
+//
+//  Created by Adi Amoyal on 12/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Cryptocurrency_Price_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
